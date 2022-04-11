2556774ce0305e88f216e47c6986d7bd45e621b0
GuillaumeDonzeau <gdonzeau@hotmail.com>
2022-04-11 16:13:29 +0200

    Testing

--- begin/MVVMFromMVC/Controllers/WeatherViewController.swift
+++ begin/MVVMFromMVC/Controllers/WeatherViewController.swift
@@ -29,18 +29,22 @@
 import UIKit
 
 class WeatherViewController: UIViewController {
-  private let geocoder = LocationGeocoder()
-  private let defaultAddress = "McGaheysville, VA"
+  private let viewModel = WeatherViewModel()
+  /*
   private let dateFormatter: DateFormatter = {
     let dateFormatter = DateFormatter()
     dateFormatter.dateFormat = "EEEE, MMM d"
     return dateFormatter
   }()
+  */
+  
+  /*
   private let tempFormatter: NumberFormatter = {
     let tempFormatter = NumberFormatter()
     tempFormatter.numberStyle = .none
     return tempFormatter
   }()
+  */
   
   @IBOutlet weak var cityLabel: UILabel!
   @IBOutlet weak var dateLabel: UILabel!
@@ -48,7 +52,33 @@ class WeatherViewController: UIViewController {
   @IBOutlet weak var currentSummaryLabel: UILabel!
   @IBOutlet weak var forecastSummary: UITextView!
   
+  @IBAction func promptForLocation(_ sender: UIButton) {
+    //1
+    let alert = UIAlertController(
+      title: "Choose location",
+      message: nil,
+      preferredStyle: .alert)
+    alert.addTextField()
+    //2
+    let submitAction = UIAlertAction(
+      title: "Submit",
+      style: .default) { [unowned alert, weak self] _ in
+        guard let newLocation = alert.textFields?.first?.text else { return }
+        self?.viewModel.changeLocation(to: newLocation)
+    }
+    alert.addAction(submitAction)
+    //3
+    present(alert, animated: true)
+  }
+  
   override func viewDidLoad() {
+    /*
+    let test = Test()
+    let variable = 12
+    let variable2 = 14
+    test.funcTest(first: variable, second: variable2)
+    */
+    /*
     geocoder.geocode(addressString: defaultAddress) { [weak self] locations in
       guard
         let self = self,
@@ -59,8 +89,28 @@ class WeatherViewController: UIViewController {
       self.cityLabel.text = location.name
       self.fetchWeatherForLocation(location)
     }
+    */
+    viewModel.locationName.bind { [weak self] locationName in
+      self?.cityLabel.text = locationName
+    }
+    viewModel.date.bind { [weak self] date in
+      self?.dateLabel.text = date
+    }
+    viewModel.icon.bind { [weak self] image in
+      self?.currentIcon.image = image
+    }
+        
+    viewModel.summary.bind { [weak self] summary in
+      self?.currentSummaryLabel.text = summary
+    }
+        
+    viewModel.forecastSummary.bind { [weak self] forecast in
+      self?.forecastSummary.text = forecast
+    }
+
   }
   
+  /*
   func fetchWeatherForLocation(_ location: Location) {
     //1
     WeatherbitService.weatherDataForLocation(
@@ -83,4 +133,5 @@ class WeatherViewController: UIViewController {
       self.forecastSummary.text = "\nSummary: \(weatherData.description)"
     }
   }
+  */
 }
